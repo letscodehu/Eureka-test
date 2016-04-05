58bbd71691053c7238336bc0ed8d513427c2e5a8
Papp Krisztián <fejlesztes@letscode.hu>
2016-04-05 23:43:53 +0200

    Eureka added.

--- app.js
+++ app.js
@@ -2,6 +2,29 @@
  * Created by tacsi on 2016. 04. 03..
  */
 var express = require('express');
+var eureka = require("eureka-js-client").Eureka;
+
+
+var euClient = new eureka({
+    // application instance information
+    instance: {
+        app: 'rucsokservice',
+        hostName: 'localhost',
+        ipAddr: '127.0.0.1',
+        port: 5000,
+        vipAddress: 'jq.test.something.com',
+        dataCenterInfo: {
+            name: 'MyOwn'
+        }
+    },
+    eureka: {
+        // eureka server host / port
+        host: '192.168.0.6',
+        port: 32874
+    }
+});
+
+euClient.start();
 
 var app = express();
 
