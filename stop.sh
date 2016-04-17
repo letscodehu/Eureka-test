5a66c22a2253361fb0c58a9d8bd650e3c9cb3b08
root <root@devel.letscode.hu>
2016-04-17 13:15:26 +0200

    Startscript modifications

---
+++ stop.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+docker rm -f accounts_mysql products_mysql orders_mysql accounts-redis orders-redis products-redis product_1 product_2 product_3 order_1 order_2 order_3 account_1 account_2 account_3 balancer_1 aggregator_1 aggregator_2
